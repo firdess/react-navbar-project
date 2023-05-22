ee2def89ae4c5de33aaec22d083ed3e92e67e93b
firdess <firdes.sahin92@gmail.com>
2023-05-22 16:44:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './Navbar'
+import './styles/styles.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import logo from './logo.svg'
+import { links, social } from './data'
+
+export default function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  }
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+
+  }, [showLinks])
+
+  return (
+    <nav>
+      <div className='navbar-center'>
+        <div className='nav-header'>
+          <img src={logo} alt='logo' className='logo' />
+          <button
+            className={`${showLinks ? 'nav-toggle active-toggle' :'nav-toggle'}`}
+            onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {
+              links.map((link) => {
+                const { id, url, text } = link;
+                return <li key={id}><a href={url}>{text}</a></li>
+              })
+
+            }
+          </ul>
+        </div>
+        <ul className='social-icons'>
+          {
+            social.map((socialIcon) => {
+              const { id, url, icon } = socialIcon;
+              return (
+                <li key={id}>
+                  <a href={url}>{icon}</a>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    </nav>
+  )
+}
